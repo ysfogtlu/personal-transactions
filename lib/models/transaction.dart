3fed14da0b0ed7dec81142e41a044b584b8d03a2
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2023-01-15 15:09:37 +0300

    data storage has been provided

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class Transaction {
   final String id;
   final String title;
@@ -12,4 +10,17 @@ class Transaction {
     required this.amount,
     required this.date,
   });
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'amount': amount,
+    'date': date.toIso8601String()
+  };
+
+  Transaction.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        amount = json['amount'],
+        date = DateTime.parse(json['date']);
 }
